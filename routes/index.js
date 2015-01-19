0e6a87769bf2abdfedc78f9295c94859d6b21955
yulonh <421998192@qq.com>
2015-01-20 01:44:34 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -12,28 +12,36 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
     var base64 = req.body['base64'];
-    getTextFromBase64(base64, function(text) {
-        res.json({
-            text: text
+    try {
+        getTextFromBase64(base64, function(text) {
+            res.json({
+                text: text
+            });
         });
-    });
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 function getTextFromBase64(base64, callback) {
     base64 = base64.replace(/^data:image\/\w+;base64,/, "")
     var bitmap = new Buffer(base64, 'base64');
-    fs.writeFileSync(imgPath, bitmap);
+    fs.writeFileSync('code.png', bitmap);
 
     var tesseract = spawn('tesseract', ['code.png', 'stdout', '-l eng', '-psm 7', 'digits']);
     tesseract.stdout.setEncoding('utf8');
-    var result = '';
+    var result;
     tesseract.stdout.on('data', function(data) {
         console.log(data);
-        result += data;
+        if (data) {
+            result += data;
+        }
     });
     tesseract.stdout.on('end', function(data) {
-        result += data;
-        //callback && callback(result);
+        if (data) {
+            result += data;
+        }
+        callback && callback(result || '');
     });
 
     tesseract.on('error', function(e) {
